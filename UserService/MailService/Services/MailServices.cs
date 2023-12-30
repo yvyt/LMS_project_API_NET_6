912d6a736a51be86f9bae40befd722c3fecba95c
Tzy <vytuong2903@gmail.com>
2023-12-30 18:10:54 +0700

    fix 2st

---
+++ UserService/MailService/Services/MailServices.cs
@@ -0,0 +1,59 @@
+﻿using MailKit.Net.Smtp;
+using MailService.Models;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailService.Services
+{
+    public class MailServices : IMailServices
+    {
+        private readonly MailSettings _emailConfig;
+        public MailServices(MailSettings emailConfig)
+        {
+            _emailConfig = emailConfig;
+        }
+        public void SendEmail(Message message)
+        {
+            var emailMessage = CreateEmail(message);
+            SendEmail(emailMessage);
+        }
+        private MimeMessage CreateEmail(Message message)
+        {
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress("email", _emailConfig.From));
+            emailMessage.To.AddRange(message.To);
+            emailMessage.Subject = message.Subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text)
+            {
+                Text = message.Content
+            };
+            return emailMessage;
+        }
+        private void SendEmail(MimeMessage mailMessage)
+        {
+            using var client = new SmtpClient();
+            try
+            {
+                client.Connect(_emailConfig.SmtpServer, _emailConfig.Port, true);
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+                client.Authenticate(_emailConfig.UserName, _emailConfig.Password);
+
+                client.Send(mailMessage);
+            }
+            catch
+            {
+                //log an error message or throw an exception or both.
+                throw;
+            }
+            finally
+            {
+                client.Disconnect(true);
+                client.Dispose();
+            }
+        }
+    }
+}
